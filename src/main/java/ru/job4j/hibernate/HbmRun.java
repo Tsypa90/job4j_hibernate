54b8ef12cd8947c51e1204fb4fb59d05acd2aa42
Tsypa90 <URSA2004@mail.ru>
2022-08-02 19:44:44 +0300

    1. ToMany [#301848]

--- src/main/java/ru/job4j/hibernate/HbmRun.java
+++ src/main/java/ru/job4j/hibernate/HbmRun.java
@@ -5,9 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.query.Query;
-
-import java.util.stream.IntStream;
 
 public class HbmRun {
     public static void main(String[] args) {
@@ -20,27 +17,28 @@ public class HbmRun {
                     .buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            Candidate one = Candidate.of("Pavel", "java", 3500);
-            Candidate two = Candidate.of("Pavel", "java", 3500);
-            Candidate three = Candidate.of("Andrey", "python", 3400);
+
+            Model one = Model.of("Prius");
+            Model two = Model.of("Camry");
+            Model three = Model.of("Hiace");
+            Model four = Model.of("Allion");
+            Model five = Model.of("Land Cruiser");
             session.save(one);
             session.save(two);
             session.save(three);
-            session.createQuery("from Candidate").list().forEach(System.out::println);
-            session.createQuery("update Candidate c set c.name = :fName, c.experience = :fExp, c.salary = :fSalary where c.id = :fId")
-                    .setParameter("fName", "Igor")
-                    .setParameter("fExp", "C#")
-                    .setParameter("fSalary", 2800.00)
-                    .setParameter("fId", 2)
-                    .executeUpdate();
-            Query query = session.createQuery("from Candidate c where c.id = :fId");
-            query.setParameter("fId", 2);
-            System.out.println(query.uniqueResult());
-            session.createQuery("from Candidate c where c.name = :fName")
-                    .setParameter("fName", "Pavel")
-                            .list().forEach(System.out::println);
-            IntStream.range(1, 4).forEach(s -> session.createQuery("delete Candidate c where c.id = :fId")
-                        .setParameter("fId", s).executeUpdate());
+            session.save(four);
+            session.save(five);
+
+            Brand brand = Brand.of("Toyota");
+
+            brand.addModel(one);
+            brand.addModel(two);
+            brand.addModel(three);
+            brand.addModel(four);
+            brand.addModel(five);
+
+            session.save(brand);
+
             session.getTransaction().commit();
             session.close();
         } catch (Exception e) {
